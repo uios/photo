8702006203e3b50b28efe296a01dca920676bf33
thebanon <91961816+thebanon@users.noreply.github.com>
2022-08-31 17:26:07 -0400

    Debug Search Results

--- app/cdn/js/on.js
+++ app/cdn/js/on.js
@@ -437,8 +437,8 @@ window.on.focus.in.search = (target)=>{
     byId('cancel-results').classList.remove('display-none');
     const keywords = byId('keywords').value;
     var goto = (window.location.pathname + '?keywords') + (keywords.length > 0 ? '=' + keywords : '') + (window.location.hash ? '#' + window.location.hash : '');
-    history.pushState(goto, '', goto);
     searchResults(keywords);
+    history.pushState(goto, '', goto);
 }
 window.on.focus.out = {};
 window.on.focus.out.search = (target)=>{
@@ -460,53 +460,59 @@ window.on.keyup.search = async(target)=>{
     const feed = byId('feed-results');
     const keywords = byId('keywords').value;
     var goto = (window.location.pathname + '?keywords') + (keywords.length > 0 ? '=' + keywords : '');
-    history.pushState(goto, '', goto);
     searchResults(keywords);
+    history.pushState(goto, '', goto);
 }
 
 function searchResults(keywords) {
     const feed = byId('feed-results');
     if (keywords) {
-        var endpoint = is.local(window.location.href) ? window.location.protocol + "//api.uios.tld" : api.endpoint;
-        const t = (d)=>{
-            const data = JSON.parse(d);
+        const param = new URLSearchParams(window.location.search).get('keywords');
+        if (feed.dataset.keywords !== keywords) {
+            var endpoint = is.local(window.location.href) ? window.location.protocol + "//api.uios.tld" : api.endpoint;
+            const t = (d)=>{
+                const data = JSON.parse(d);
 
-            feed.innerHTML = "";
-
-            const users = data.users;
-            if (users && users.length > 0) {
-                const template = byId('template-results-user').content.firstElementChild;
-                var u = 0;
-                do {
-                    const user = users[u];
-                    const uid = user.uid;
-
-                    var html = template.cloneNode(true);
-
-                    html.dataset.href = "/users/" + user.username + "/";
-                    html.classList.remove('hide');
-                    html.dataset.uid = uid;
-                    html.find('[placeholder="username"]').textContent = user.username;
-                    html.find('[placeholder="Full Name"]').textContent = user.fullname;
-
-                    feed.insertAdjacentHTML('beforeend', html.outerHTML);
-                    u++;
-                } while (u < users.length);
-            } else {
                 feed.innerHTML = "";
+                feed.dataset.keywords = keywords;
+
+                const users = data.users;
+                if (users && users.length > 0) {
+                    const template = byId('template-results-user').content.firstElementChild;
+                    var u = 0;
+                    do {
+                        const user = users[u];
+                        const uid = user.uid;
+
+                        var html = template.cloneNode(true);
+
+                        html.dataset.href = "/users/" + user.username + "/";
+                        html.classList.remove('hide');
+                        html.dataset.uid = uid;
+                        html.find('[placeholder="username"]').textContent = user.username;
+                        html.find('[placeholder="Full Name"]').textContent = user.fullname;
+
+                        feed.insertAdjacentHTML('beforeend', html.outerHTML);
+                        u++;
+                    } while (u < users.length);
+                } else {
+                    feed.innerHTML = "";
+                }
+                resolve(route);
             }
-            resolve(route);
-        }
-        const c = ()=>{}
+            const c = ()=>{}
 
-        if (window.abort) {
-            console.log("abosrting");
-            window.abort.abort()
-        }
-        window.abort = new AbortController()
-        window.signal = window.abort.signal
+            if (window.yield) {
+                console.log("abosrting");
+                window.yield.abort()
+            }
+            window.yield = new AbortController()
+            window.signal = window.yield.signal
 
-        ajax(endpoint + '/v1/search?keywords=' + keywords, {signal}).then(t).catch(c);
+            ajax(endpoint + '/v1/search?keywords=' + keywords, {
+                signal
+            }).then(t).catch(c);
+        }
     } else {
         feed.innerHTML = "";
     }
